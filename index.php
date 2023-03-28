52a2c7baba27f4aa754adb443bf050883af8616c
Hamed <hamed@Hameds-Mac-mini.local>
2023-03-28 17:05:33 +0330

    stable

--- index.php
+++ index.php
@@ -1,9 +1,133 @@
 <?php get_header();?>
 
-<div class="sepid-entry home text-left" id="home">
-
-<button class="bg-yellow-800">click me</button>
-
+<div
+        class="w-[1600px] h-[166px] relative border-t-0 border-r-0 border-b-2 border-l-0 border-[#efefef]"
+>
+    <div class="w-[164px] h-[52px] absolute left-[1358px] top-[29px]">
+        <p class="absolute left-0 top-0 text-[29px] font-black text-left">
+      <span class="text-[29px] font-black text-left text-[#d73868]">بهـسیما</span
+      ><span class="text-[29px] font-black text-left text-[#333]">شاپ</span>
+        </p>
+    </div>
+    <div class="w-[29px] h-[30.59px]">
+        <svg
+                width="21"
+                height="27"
+                viewBox="0 0 21 27"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                class="absolute left-[91.5px] top-[38.5px]"
+                preserveAspectRatio="none"
+        >
+            <path
+                    d="M19.0476 6.55263H15.1378V5.46053C15.1378 2.40263 12.9323 0 10.1253 0C7.31827 0 5.11277 2.40263 5.11277 5.46053V10.9211H7.11777V8.73684H11.1278V6.55263H7.11777V5.46053C7.11777 3.60395 8.42103 2.18421 10.1253 2.18421C11.8295 2.18421 13.1328 3.60395 13.1328 5.46053V10.9211H15.1378V8.73684H17.2431L18.1453 24.0263H2.20551L3.00751 8.73684H3.10776V6.55263H1.203L0 26.2105H20.1503L19.0476 6.55263Z"
+                    fill="#4C515B"
+            ></path>
+        </svg>
+        <div class="w-[17px] h-[18px]">
+            <svg
+                    width="17"
+                    height="18"
+                    viewBox="0 0 17 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="absolute left-[103.5px] top-[51.09px]"
+                    preserveAspectRatio="xMidYMid meet"
+            >
+                <circle cx="8.5" cy="9.0921" r="8.5" fill="#D73868"></circle>
+            </svg>
+            <p class="absolute left-[111px] top-[51.59px] text-[10px] font-black text-left text-white">
+                ۱
+            </p>
+        </div>
+    </div>
+    <p class="absolute left-[152px] top-10 text-sm font-medium text-left text-[#212121]">
+        ورود / ثبت نام
+    </p>
+    <div class="w-[570px] h-[46px]">
+        <div
+                class="w-[570px] h-[46px] absolute left-[776.5px] top-[29.5px] rounded-xl bg-[#f2f2f2]"
+        ></div>
+        <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-6 h-6 absolute left-[1310px] top-[41px]"
+                preserveAspectRatio="none"
+        >
+            <path
+                    d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
+                    stroke="#999999"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+            ></path>
+            <path
+                    d="M21 21L16.65 16.65"
+                    stroke="#999999"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+            ></path>
+        </svg>
+        <p class="absolute left-[1178px] top-10 text-sm text-left text-[#585858]">
+            جستجو در فروشگاه...‍
+        </p>
+    </div>
+    <div
+            class="flex justify-start items-center w-[171px] absolute left-20 top-[101px] gap-2 p-2.5 rounded-xl bg-[#d73868]"
+    >
+        <div class="flex-grow h-[25px] relative">
+            <p class="w-[133px] absolute left-[-7px] top-0 text-sm font-medium text-center text-white">
+                تخفیف تکرار نشدنی
+            </p>
+        </div>
+        <svg
+                width="24"
+                height="25"
+                viewBox="0 0 24 25"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                class="flex-grow-0 flex-shrink-0 w-6 h-6 relative"
+                preserveAspectRatio="none"
+        >
+            <path
+                    d="M22 8.05C22 8.71 21.46 9.25 20.8 9.25H3.2C2.54 9.25 2 8.71 2 8.05V8.04C2 5.75 3.85 3.9 6.14 3.9H17.85C20.14 3.9 22 5.76 22 8.05Z"
+                    fill="white"
+            ></path>
+            <path
+                    d="M2 11.95V16.96C2 19.25 3.85 21.1 6.14 21.1H17.85C20.14 21.1 22 19.24 22 16.95V11.95C22 11.29 21.46 10.75 20.8 10.75H3.2C2.54 10.75 2 11.29 2 11.95ZM8 17.75H6C5.59 17.75 5.25 17.41 5.25 17C5.25 16.59 5.59 16.25 6 16.25H8C8.41 16.25 8.75 16.59 8.75 17C8.75 17.41 8.41 17.75 8 17.75ZM14.5 17.75H10.5C10.09 17.75 9.75 17.41 9.75 17C9.75 16.59 10.09 16.25 10.5 16.25H14.5C14.91 16.25 15.25 16.59 15.25 17C15.25 17.41 14.91 17.75 14.5 17.75Z"
+                    fill="white"
+            ></path>
+        </svg>
+    </div>
+    <div class="flex justify-end items-center absolute left-[828px] top-[111px] gap-6">
+        <p class="flex-grow-0 flex-shrink-0 w-[99px] text-sm text-right text-[#212121]">فروش همکار</p>
+        <p class="flex-grow-0 flex-shrink-0 text-sm text-left text-[#212121]">تماس با کارشناس</p>
+        <p class="flex-grow-0 flex-shrink-0 text-sm text-left text-[#212121]">بیوتی بلاگ</p>
+        <p class="flex-grow-0 flex-shrink-0 text-sm text-left text-[#212121]">لوازم برقی</p>
+        <p class="flex-grow-0 flex-shrink-0 text-sm text-left text-[#212121]">مو</p>
+        <p class="flex-grow-0 flex-shrink-0 text-sm text-left text-[#212121]">آرایشی</p>
+        <p class="flex-grow-0 flex-shrink-0 text-sm text-left text-[#212121]">بهداشتی</p>
+        <p class="flex-grow-0 flex-shrink-0 text-sm text-left text-[#212121]">عطر</p>
+        <p class="flex-grow-0 flex-shrink-0 text-sm text-left text-[#212121]">برندها</p>
+    </div>
+    <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-4 h-4"
+            preserveAspectRatio="none"
+    >
+        <circle cx="3.5" cy="3.5" r="3.5" fill="#D73868"></circle>
+        <circle cx="12.5" cy="3.5" r="3.5" fill="#D73868"></circle>
+        <circle cx="3.5" cy="12.5" r="3.5" fill="#D73868"></circle>
+        <circle cx="12.5" cy="12.5" r="3.5" fill="#D73868"></circle>
+    </svg>
 </div>
 
 <?php get_footer();?>
